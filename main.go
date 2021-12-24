85179dc635463729b4b1e2c5a658aff2b9efa551
n.pendurin <n.pendurin@telecontact.ru>
2021-12-24 13:29:03 +0300

    -- Added loop termination when client disconnects

--- main.go
+++ main.go
@@ -13,12 +13,21 @@ type (
 	}
 )
 
-var listeners map[string] *Client
-var counter int 
+var listeners map[string]*Client
+var counter int
 
 func main() {
-	listeners = make(map[string] *Client)
+	listeners = make(map[string]*Client)
 	go loop()
+	go func() {
+		for {
+			fmt.Println("listeners:")
+			for key := range listeners {
+				fmt.Printf("\tlistener: %s\n", key)
+			}
+			time.Sleep(5 * time.Second)
+		}
+	}()
 
 	http.Handle("/", http.FileServer(http.Dir("")))
 	http.HandleFunc("/sse", handler)
@@ -30,8 +39,10 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	connected := true
+
 	fmt.Printf("%s connected\n", r.RemoteAddr)
-	if listeners[r.RemoteAddr] == nil{
+	if listeners[r.RemoteAddr] == nil {
 		listeners[r.RemoteAddr] = &Client{r.RemoteAddr}
 	}
 	w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -40,19 +51,27 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 
-	for {
+	for ;connected; {
 		messageID := uint(rand.Uint32())
 		fmt.Fprintf(w, "data: %d\nid:%d\n\n", counter, messageID)
-		
+
 		if f, ok := w.(http.Flusher); ok {
 			f.Flush()
 		}
-		time.Sleep(5 * time.Second)
+		
+		select {
+		case <-r.Context().Done():
+			connected = false
+			delete(listeners, r.RemoteAddr)
+			fmt.Printf("%s close connection\n", r.RemoteAddr)
+		default:
+			time.Sleep(5 * time.Second)
+		}
 	}
 }
 
 func loop() {
-	for counter = 0;; counter++ {
+	for counter = 0; ; counter++ {
 		time.Sleep(10 * time.Second)
 	}
 }
